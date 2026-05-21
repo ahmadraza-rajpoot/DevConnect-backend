60dea817446cbc53f36e7c306a228aa2db33bc45
Ahmad Raza <razarajpoot99598@gmail.com>
2026-05-22 03:48:55 +0500

    temporarily update the db

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const {URI} = require("../../secret.js")
 
 const connectDB = async ()=>{
- await mongoose.connect(URI)
+ await mongoose.connect("mongodb://raza:MhO428dPMUEPYuv5@ac-fyzkrz0-shard-00-00.purvy8z.mongodb.net:27017,ac-fyzkrz0-shard-00-01.purvy8z.mongodb.net:27017,ac-fyzkrz0-shard-00-02.purvy8z.mongodb.net:27017/node?ssl=true&replicaSet=atlas-nw8y26-shard-0&authSource=admin&appName=store/node")
 }
 
 
